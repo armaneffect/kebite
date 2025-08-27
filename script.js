004f2cdbb082687b682196afe26287dd552e6493
Midland Computer <aharmankhan333@gmail.com>
2025-08-27 12:44:42 +0600

    final commit 3

--- script.js
+++
@@ -1,6 +0,0 @@
-const openbtn = document.querySelector(".openbtn");
-const menulist = document.querySelector(".menulist");
-
-openbtn.addEventListener("click", () => {
-    menulist.classList.toggle("hidden");
-});
